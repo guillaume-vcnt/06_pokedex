40837f7dac8b9cbb927aee88f1cf02cdb811634b
Guillaume <anonyme@example.com>
2025-02-26 18:27:39 +0100

    update

--- with_module/script_module.js
+++
@@ -1,8 +0,0 @@
-import { pokedex } from "../with_module/pokedex.js";
-console.log(pokedex);
-
-const numberPokemon = pokedex.pokemon.length;
-console.log("Nombre de Pokemon", numberPokemon);
-
-const pokemonNames = pokedex.pokemon.map((elm) => elm.name);
-console.log("Noms des Pokemon:", pokemonNames);
